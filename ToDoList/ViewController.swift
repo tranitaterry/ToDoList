20d1d69888d75d57bf3add06924faf073136bca2
Tranita Terry <tranitacharterry@icloud.com>
2020-11-12 22:31:11 -0600

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by MXC Swift  on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
